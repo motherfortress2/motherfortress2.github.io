acfa9ff4566e257c782b1c1b5a3c53953c042529
SpaceWD <121475790+spacewd69@users.noreply.github.com>
2024-08-30 20:43:46 -0400

    Update alerts.js

--- alerts.js
+++ alerts.js
@@ -5,47 +5,10 @@ function showAlert() {
 	hideAlert();
 	document.getElementById("messagebox").style.display = "table";
 	document.getElementById("line1").style.display = "inline";
-	document.getElementById("line1").innerHTML = "<br>Website by mirzi and Czechball";
+	document.getElementById("line1").innerHTML = "<br>Website source code by mirzi and Czechball";
 	document.getElementById("githublink").style.display = "inline";
 	document.getElementById("okbutton").display = "inline";
 }
-
-/* most of this code has been replaced with the new server browser
-//minecraft server info
-function showAlertMc() {
-	hideAlert();
-	document.getElementById("messagebox").style.display = "table";
-	document.getElementById("line1").style.display = "inline";
-	document.getElementById("line1").innerHTML = "<br>Join our minecraft server!";
-	document.getElementById("line2").style.display = "inline";
-	IP = "mc.lambdaposting.games";
-	document.getElementById("line2").innerHTML = "IP: " +IP;
-	document.getElementById("copybutton").style.display = "inline";
-	document.getElementById("okbutton").display = "inline";
-}
-
-//event info
-function showAlertEvent() {
-	hideAlert();
-	document.getElementById("messagebox").style.display = "none";
-	document.getElementById("line1").style.display = "inline";
-	document.getElementById("line1").innerHTML = "<br>" + eventTitle;
-	IP = eventIP;
-	document.getElementById("line2").style.display = "inline";
-	document.getElementById("line2").innerHTML = "IP: " +IP;
-	document.getElementById("copybutton").style.display = "inline";
-	document.getElementById("okbutton").display = "inline";
-}
-*/
-
-function showServerBrowser(){
-	document.getElementById("serverbrowser").style.display = "table";
-}
-
-function hideServerBrowser(){
-	document.getElementById("serverbrowser").style.display = "none";
-}
-
 //hide all - runs on page load
 function hideAlert() {
 	document.getElementById("messagebox").style.display = "none";
